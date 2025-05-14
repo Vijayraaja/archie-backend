02e80f56955b0537fd2287c8ba74a8e9528cb921
Vijayraaja <vijayraaja@gmail.com>
2025-05-14 16:24:05 +0530

    Delete archie-frontend directory

--- archie-frontend/src/components/ui/input.jsx
+++
@@ -1,8 +0,0 @@
-export function Input({ ...props }) {
-  return (
-    <input
-      {...props}
-      className="flex h-10 w-full rounded-md border px-3 py-2 text-sm shadow-sm outline-none"
-    />
-  );
-}
