a8cee0f1fefcb0131eed0372d5316fd45da71e83
Vijayraaja <vijayraaja@gmail.com>
2025-05-14 16:15:45 +0530

    Delete archie-frontend/src directory

--- archie-frontend/src/components/ui/scroll-area.jsx
+++
@@ -1,7 +0,0 @@
-export function ScrollArea({ children, className = '' }) {
-  return (
-    <div className={`overflow-y-auto scrollbar-thin scrollbar-thumb-gray-400 ${className}`}>
-      {children}
-    </div>
-  );
-}
